422bfc9e55a1b95c970d39005c2dbfeff61faf10
sritam628 <sritamgiri628@gmail.com>
2025-07-12 19:57:41 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+name = "Devops"
+count = 5
+price = 100.0
+active = True
+
+print(name)
+print(count)
+print(price)
+print(active)
+
+log = "success"
+
+if log == "success":
+    print("Log is successful")
+else:
+    print("Log failed")
