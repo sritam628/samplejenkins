6c3455c20e25628b3d15b94ab9d08eb39ce5400e
sritam628 <sritamgiri628@gmail.com>
2025-07-12 21:44:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from datetime import datetime, timedelta
+now = datetime.now()
+
+# Print the current date and time
+print(now)
+
+today = now.date()
+print(today)
+
+
+fromatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+print("Formatted Date:", )
